30167c1d9d2933990c4787829d7313cd3ba25c99
Your Name <kingdarkungel000@gmail.com>
2026-07-28 11:07:45 +0500

    Fix broken fallback Unsplash images and clean up next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: '**' }
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'kfcyanoqdydwpjrdsdvc.supabase.co' }
     ],
   },
 };
